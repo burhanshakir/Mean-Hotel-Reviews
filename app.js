e30afe1e258cad1746a2a3a7a2aafb12453b164f
bshakir <burhanshakir96@gmail.com>
2018-07-18 15:31:54 +0100

    Adding routes

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
+var routes = require('./api/routes');
 
 // Define the port to run on
 app.set('port', 3000);
 
+// Add middleware to console log every request
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next(); 
+});
+
 // Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enable parsing of posted forms
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Add some routing
-
-app.get('/json', function(req, res) {
-  console.log("GET the json");
-  res
-    .status(200)
-    .json( {"jsonData" : true} );
-});
-
-app.get('/file', function(req, res) {
-  console.log("GET the file");
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, 'app.js'));
-});
-
+app.use('/api', routes);
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
