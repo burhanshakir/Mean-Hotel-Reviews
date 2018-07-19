6f2ee296a2d8812fab624621354ec10324d19fb5
bshakir <burhanshakir96@gmail.com>
2018-07-19 16:30:02 +0100

    Making api robust

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,57 +1,138 @@
-var dbconn = require('../data/dbconnection.js');
-var ObjectId = require('mongodb').ObjectID;
+var mongoose = require('mongoose');
+var Hotel = mongoose.model('Hotel');
 
-var hotelData = require('../data/hotel-data.json');
+
+var runGeoQuery = function(req, res) {
+
+  var lng = parseFloat(req.query.lng);
+  var lat = parseFloat(req.query.lat);
+
+  if (isNaN(lng) || isNaN(lat)) {
+    res
+      .status(400)
+      .json({
+        "message" : "If supplied in querystring, lng and lat must both be numbers"
+      });
+    return;
+  }
+
+  // A geoJSON point
+  var point = {
+    type : "Point",
+    coordinates : [lng, lat]
+  };
+
+  var geoOptions = {
+    spherical : true,
+    maxDistance : 2000,
+    num : 5
+  };
+
+  Hotel
+    .geoNear(point, geoOptions, function(err, results, stats) {
+      console.log('Geo Results', results);
+      console.log('Geo stats', stats);
+      if (err) {
+        console.log("Error finding hotels");
+        res
+          .status(500)
+          .json(err);
+      } else {
+        res
+          .status(200)
+          .json(results);
+      }
+    });
+};
 
 module.exports.hotelsGetAll = function(req, res) {
-
-  var db = dbconn.get();
-
-  // console.log("db", db);
 
   console.log('GET the hotels');
   console.log(req.query);
 
   var offset = 0;
   var count = 5;
+  var maxCount = 50;
 
-  var collection = db.collection('hotels');
+  if (req.query && req.query.lat && req.query.lng) {
+    runGeoQuery(req, res);
+    return;
+  }
 
-  if (req.query && req.query.offset) {
+if (req.query && req.query.offset) {
     offset = parseInt(req.query.offset, 10);
   }
-  
+
   if (req.query && req.query.count) {
     count = parseInt(req.query.count, 10);
   }
 
-  collection
+  if (isNaN(offset) || isNaN(count)) {
+    res
+      .status(400)
+      .json({
+        "message" : "If supplied in querystring, count and offset must both be numbers"
+      });
+    return;
+  }
+
+  if (count > maxCount) {
+    res
+      .status(400)
+      .json({
+        "message" : "Count limit of " + maxCount + " exceeded"
+      });
+    return;
+  }
+
+  Hotel
     .find()
     .skip(offset)
     .limit(count)
-    .toArray(function(err, docs) {
-      console.log("Found hotels", docs.length);
-      res
-        .status(200)
-        .json(docs);
-  });
+    .exec(function(err, hotels) {
+      console.log(err);
+      console.log(hotels);
+      if (err) {
+        console.log("Error finding hotels");
+        res
+          .status(500)
+          .json(err);
+      } else {
+        console.log("Found hotels", hotels.length);
+        res
+          .json(hotels);
+      }
+    });
 
 };
 
 module.exports.hotelsGetOne = function(req, res) {
-  var db = dbconn.get();
   var id = req.params.hotelId;
-  var collection = db.collection('hotels');
+
   console.log('GET hotelId', id);
 
-  collection
-    .findOne({
-      _id : ObjectId(id)
-    }, function(err, doc) {
+  Hotel
+    .findById(id)
+    .exec(function(err, doc) {
+      var response = {
+        status : 200,
+        message : doc
+      };
+      if (err) {
+        console.log("Error finding hotel");
+        response.status = 500;
+        response.message = err;
+      } else if(!doc) {
+        console.log("HotelId not found in database", id);
+        response.status = 404;
+        response.message = {
+          "message" : "Hotel ID not found " + id
+        };
+      }
       res
-        .status(200)
-        .json(doc);
-  });
+        .status(response.status)
+        .json(response.message);
+    });
 
 };
 
@@ -79,7 +160,9 @@ module.exports.hotelsAddOne = function(req, res) {
     console.log("Data missing from body");
     res
       .status(400)
-      .json({ message : "Required data missing from body" });
+      .json({
+        message: "Required data missing from body"
+      });
   }
 
 };
