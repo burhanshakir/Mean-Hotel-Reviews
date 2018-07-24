7dd52ebfe0ea104082222d0fe93464aad3c6a75a
bshakir <burhanshakir96@gmail.com>
2018-07-24 11:44:06 +0100

    Adding form

--- public/angular-app/hotel-display/hotel-controller.js
+++ public/angular-app/hotel-display/hotel-controller.js
@@ -1,11 +1,43 @@
 angular.module('meanhotel').controller('HotelController', HotelController);
 
-function HotelController($http, $routeParams){
+function HotelController($route,$window,hotelDataFactory, $routeParams){
   var vm = this;
   var params = $routeParams.id;
 
-  $http.get('/api/hotels/' + params).then(function(response) {
+  hotelDataFactory.hotelDisplay(params).then(function(response) {
     console.log(response);
     vm.hotel = response.data;
+    vm.stars = _getStarRating(response.data.stars);
   });
+
+  function _getStarRating(stars) {
+
+    return new Array(stars);
+
+  }
+
+  vm.addReview = function() {
+
+    // var token = jwtHelper.decodeToken($window.sessionStorage.token);
+    // var username = token.username;
+
+    var postData = {
+      name: 'Burhan',
+      rating: vm.rating,
+      review: vm.review
+    };
+
+    if (vm.reviewForm.$valid) {
+      hotelDataFactory.postReview(params, postData).then(function(response) {
+        console.log(response);
+        if (response.status === 201) {
+          $route.reload();
+        }
+      }).catch(function(error) {
+        console.log(error);
+      });
+    } else {
+      vm.isSubmitted = true;
+    }
+  };
 }
